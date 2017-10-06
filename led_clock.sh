97e2b968e065c1ec44d1a8fb7ac3d803b7a147ae
Pi Volta <mythvolta@github.com>
2017-10-06 13:06:10 -0400

    Fix the script so that it can be run anywhere.

--- led_clock.sh
+++ led_clock.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Run the clock on the LED matrix
-sudo ~pi/code/rpi-rgb-led-matrix/examples-api-use/clock -f ../fonts/6x13B.bdf --led-rows=16 -b3 -x1 -y-1
+LED=~pi/code/rpi-rgb-led-matrix
+sudo $LED/examples-api-use/clock -f $LED/fonts/6x13B.bdf -b3 -x1 -y-1
