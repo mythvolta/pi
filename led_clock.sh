5879660e01deeeda39b4f344d7854e9d1366d2fc
Pi Volta <mythvolta@github.com>
2017-10-07 20:37:41 -0400

    Better clock.

--- led_clock.sh
+++ led_clock.sh
@@ -2,4 +2,5 @@
 
 # Run the clock on the LED matrix
 LED=~pi/code/rpi-rgb-led-matrix
-sudo $LED/examples-api-use/clock -f $LED/fonts/6x13B.bdf -b3 -x1 -y-1
+#sudo $LED/examples-api-use/clock -f $LED/fonts/6x13B.bdf -b3 -x1 -y-1
+sudo .$LED/examples-api-use/clock -f $LED/fonts/6x13B.bdf -b10 -d '%H%M' --led-rows=16 -S1 -y0 -x1
