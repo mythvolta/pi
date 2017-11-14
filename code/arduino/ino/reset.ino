7932c57854d8a396279c7d315cfbeb0fc653b3ae
Pi Volta <mythvolta@github.com>
2017-11-14 15:57:46 -0500

    Added arduino code, and a good makefile. Upload with [make upload], reset with [make reset].

---
+++ code/arduino/ino/reset.ino
@@ -0,0 +1,12 @@
+// Reset the settings on the Arduino
+
+// Initialize the board
+void setup() {
+  // Don't do anything
+}
+
+// Loop forever
+void loop() {
+  // Sleep for a second
+  delay(1000);
+}
