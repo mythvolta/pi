b722c31cc1fe093384d2541922738b956075fc1f
Pi Volta <mythvolta@github.com>
2017-11-14 23:48:35 -0500

    Add serial port checks, and adjust the levels.

--- code/arduino/ino/photoresistor.ino
+++ code/arduino/ino/photoresistor.ino
@@ -11,6 +11,8 @@ const int PIN_SPEAKER = 5;
 const int PIN_MOTOR = 9;
 
 // Pull out variables for speed
+byte char_in;
+char buffer[128];
 int sensor_value = 0;
 int led_level    = 0;
 bool melody_playing = false;
@@ -27,8 +29,13 @@ int noteDurations[] = {
 
 // Functions
 void play_melody();
+void serial_port();
 
 void setup() {
+  // Enable the serial port
+  // A baud rate of 9600 (8-bit with No parity and 1 stop bit)
+  Serial.begin(9600, SERIAL_8N1);
+
   // All LEDs are outputs
   for (int led_number = 0; led_number < num_leds; ++led_number) {
     pinMode(PIN_LEDS[led_number], OUTPUT);
@@ -43,10 +50,20 @@ void loop() {
   sensor_value = analogRead(PIN_PHOTORESISTOR);
 
   // Light between 0 and 3 LEDs
-  led_level = map(sensor_value, 100, 900, 0, num_leds);  // map to the number of LEDs
+  led_level = 0;
+  if (sensor_value > 500) {
+    led_level = 3;
+  }
+  else if (sensor_value > 300) {
+    led_level = 2;
+  }
+  else if (sensor_value > 150) {
+    led_level = 1;
+  }
+
   for (int led_number = 0; led_number < num_leds; ++led_number) {
     // When led_number is smaller than led_level, run the following code.
-    if (led_level >= led_number) {
+    if (led_level > led_number) {
       digitalWrite(PIN_LEDS[led_number], HIGH);     // turn on pins less than the level
     }
     else {
@@ -58,17 +75,12 @@ void loop() {
   // Generates a square wave of the frequency of 10000 Hz (and 50% duty cycle) on a pin 5 
   if (led_level >= 2) {
     pinMode(PIN_SPEAKER, OUTPUT);
-    if (led_level >= 3) {
-      if (!melody_playing) {
-        melody_playing = true;
-        play_melody();
-      }
-    }
-    //else {
-    //  tone(PIN_SPEAKER, 10000, 200);
-    //}
+    tone(PIN_SPEAKER, 10000);
     pinMode(PIN_SPEAKER, INPUT);
   }
+
+  // Check the serial port
+  serial_port();
 }
 
 void play_melody() {
@@ -86,5 +98,18 @@ void play_melody() {
     delay(pauseBetweenNotes);
     // stop the tone playing:
     noTone(PIN_SPEAKER);
+  }
+}
+
+// Read from the serial port for debugging
+void serial_port() {
+  if (Serial.available()) {
+    // Read the character
+    char_in = Serial.read();
+
+    // Send the character back to the RPi,
+    //  along with the brightness
+    sprintf(buffer, "[%c] #%d (%d)", char_in, led_level, sensor_value);
+    Serial.println(buffer);
   }
 }
