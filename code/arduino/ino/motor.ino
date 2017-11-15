1ea9d632ef578788b519b4094a7fd911ec69550a
Pi Volta <mythvolta@github.com>
2017-11-14 21:11:48 -0500

    Motor changes, but still not working.

--- code/arduino/ino/motor.ino
+++ code/arduino/ino/motor.ino
@@ -7,41 +7,87 @@
 //Website: www.sunfounder.com
 
 #include <Servo.h>
-/************************************************/
-Servo myservo;//create servo object to control a servo
-/************************************************/
+
+// Constants
+const int PIN_POT = A1;
+const int PIN_LED = 6;
+const int PIN_MOTOR = 5;
+
+// Variables
+uint16_t pot_value = 0;
+Servo myservo; // create servo object to control a servo
+
+// Functions
+void flash_led();
+void spin_motor();
+void set_motor_speed(int speed);
+
+/*******************/
 void setup()
 {
-  myservo.attach(9);//attachs the servo on pin 9 to servo object
-  myservo.write(0);//back to 0 degrees 
-  delay(1000);//wait for a second
-}
-/*************************************************/
-void loop()
-{  
-  myservo.write(15);//goes to 15 degrees 
-  delay(1000);//wait for a second
-  myservo.write(30);//goes to 30 degrees 
-  delay(1000);//wait for a second.33
-  myservo.write(45);//goes to 45 degrees 
-  delay(1000);//wait for a second.33
-  myservo.write(60);//goes to 60 degrees 
-  delay(1000);//wait for a second.33
-  myservo.write(75);//goes to 75 degrees 
-  delay(1000);//wait for a second.33
-  myservo.write(90);//goes to 90 degrees
-  delay(1000);//wait for a second
-  myservo.write(75);//back to 75 degrees 
-  delay(1000);//wait for a second.33
-  myservo.write(60);//back to 60 degrees
-  delay(1000);//wait for a second.33
-  myservo.write(45);//back to 45 degrees
-  delay(1000);//wait for a second.33
-  myservo.write(30);//back to 30 degrees
-  delay(1000);//wait for a second.33
-  myservo.write(15);//back to 15 degrees 
-  delay(1000);//wait for a second
-  myservo.write(0);//back to 0 degrees 
-  delay(1000);//wait for a second
+  //Serial.begin(9600);
+
+  //pinMode(PIN_POT, INPUT);
+  //pinMode(PIN_LED, OUTPUT);
+  //pinMode(PIN_MOTOR, OUTPUT);
+
+  // Turn off the motor
+  //set_motor_speed(0); // Range is [0, 255]
+
+  //attachs the servo on pin 9 to servo object
+  myservo.attach(PIN_MOTOR);
+  myservo.write(0);
+
+  // Flash the LED once
+  flash_led();
 }
 /**************************************************/
+
+/*******************/
+void loop() { 
+  // Check to see if we should flash the LED
+  //flash_led();
+
+  // Read from the potentiometer
+  //pot_value = analogRead(PIN_POT);
+
+  // Spin the motor
+  spin_motor();
+}
+
+// Spin the motor fast then slow
+void spin_motor() {
+  // Drive the motor, in a loop
+  for (int speed = 0; speed <= 255; speed += 10) {
+    set_motor_speed(speed);
+    delay(500);
+  }
+  for (int speed = 255; speed >= 0; speed -= 10) {
+    set_motor_speed(speed);
+    delay(500);
+  }
+  set_motor_speed(0);
+  delay(3000);
+}
+
+// Flash the LED once
+void flash_led() {
+  // fade in from min to max in increments of 5 points:
+  for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) { 
+    analogWrite(PIN_LED, fadeValue);         
+    delay(30);                            
+  } 
+
+  // fade out from max to min in increments of 5 points:
+  for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -=5) { 
+    analogWrite(PIN_LED, fadeValue);         
+    delay(30);                            
+  }
+}
+
+// Function to set the speed of the motor
+void set_motor_speed(int speed) {
+  int motorSpeed = constrain(abs(speed), 0, 255);
+  myservo.write(motorSpeed);
+  analogWrite(PIN_LED, motorSpeed);
+}
