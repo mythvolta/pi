d14c151424cc8b5c34b9a96a234d8694d6bed607
Pi Volta <mythvolta@github.com>
2017-11-25 16:09:52 -0500

    Make changes to mythclock, and change the Makefile back to the correct program.

--- code/mythclock/mythclock.cc
+++ code/mythclock/mythclock.cc
@@ -21,104 +21,24 @@ static void InterruptHandler(int signo) {
   interrupt_received = true;
 }
 
-// Method to draw text
-void draw_text(const char *text);
-
-static int usage(const char *progname) {
-  fprintf(stderr, "usage: %s [options]\n", progname);
-  fprintf(stderr, "Reads text from stdin and displays it. "
-          "Empty string: clear screen\n");
-  fprintf(stderr, "Options:\n");
-  rgb_matrix::PrintMatrixFlags(stderr);
-  fprintf(stderr,
-          "\t-d <time-format>  : Default '%%H:%%M'. See strftime()\n"
-          "\t-f <font-file>    : Use given font.\n"
-          "\t-b <brightness>   : Sets brightness percent. Default: 100.\n"
-          "\t-x <x-origin>     : X-Origin of displaying text (Default: 0)\n"
-          "\t-y <y-origin>     : Y-Origin of displaying text (Default: 0)\n"
-          "\t-S <spacing>      : Spacing pixels between letters (Default: 0)\n"
-          "\t-C <r,g,b>        : Color. Default 255,255,0\n"
-          "\t-B <r,g,b>        : Background-Color. Default 0,0,0\n"
-          "\t-O <r,g,b>        : Outline-Color, e.g. to increase contrast.\n"
-          );
-
-  return 1;
-}
-
-static bool parseColor(Color *c, const char *str) {
-  return sscanf(str, "%hhu,%hhu,%hhu", &c->r, &c->g, &c->b) == 3;
-}
-
-static bool FullSaturation(const Color &c) {
-    return (c.r == 0 || c.r == 255)
-        && (c.g == 0 || c.g == 255)
-        && (c.b == 0 || c.b == 255);
-}
-
 int main(int argc, char *argv[]) {
-  RGBMatrix::Options matrix_options;
-  rgb_matrix::RuntimeOptions runtime_opt;
-  if (!rgb_matrix::ParseOptionsFromFlags(&argc, &argv,
-                                         &matrix_options, &runtime_opt)) {
-    return usage(argv[0]);
-  }
-
-  const char *time_format = "%H:%M";
-  Color color(255, 255, 0);
+  // All options are hardcoded
+  const std::string time_format = "%H:%M:%S";
+  Color color(128, 0, 0);
   Color bg_color(0, 0, 0);
   Color outline_color(0,0,0);
-  bool with_outline = false;
+  //bool with_outline = false;
+  const bool with_outline = true;
+  const std::string bdf_font_file = "/home/pi/code/rpi-rgb-led-matrix/fonts/6x13B.bdf";
+  const int x_orig = 0;
+  const int y_orig = 0;
+  int brightness = 20; // Changes based on time of day
+  const int letter_spacing = 0;
 
-  const char *bdf_font_file = NULL;
-  int x_orig = 0;
-  int y_orig = 0;
-  int brightness = 20;
-  int letter_spacing = 0;
-
-  int opt;
-  while ((opt = getopt(argc, argv, "x:y:f:C:B:O:b:S:d:")) != -1) {
-    switch (opt) {
-    case 'd': time_format = strdup(optarg); break;
-    case 'b': brightness = atoi(optarg); break;
-    case 'x': x_orig = atoi(optarg); break;
-    case 'y': y_orig = atoi(optarg); break;
-    case 'f': bdf_font_file = strdup(optarg); break;
-    case 'S': letter_spacing = atoi(optarg); break;
-    case 'C':
-      if (!parseColor(&color, optarg)) {
-        fprintf(stderr, "Invalid color spec: %s\n", optarg);
-        return usage(argv[0]);
-      }
-      break;
-    case 'B':
-      if (!parseColor(&bg_color, optarg)) {
-        fprintf(stderr, "Invalid background color spec: %s\n", optarg);
-        return usage(argv[0]);
-      }
-      break;
-    case 'O':
-      if (!parseColor(&outline_color, optarg)) {
-        fprintf(stderr, "Invalid outline color spec: %s\n", optarg);
-        return usage(argv[0]);
-      }
-      with_outline = true;
-      break;
-    default:
-      return usage(argv[0]);
-    }
-  }
-
-  if (bdf_font_file == NULL) {
-    fprintf(stderr, "Need to specify BDF font-file with -f\n");
-    return usage(argv[0]);
-  }
-
-  /*
-   * Load font. This needs to be a filename with a bdf bitmap font.
-   */
+  // Load font. This needs to be a filename with a bdf bitmap font.
   rgb_matrix::Font font;
-  if (!font.LoadFont(bdf_font_file)) {
-    fprintf(stderr, "Couldn't load font '%s'\n", bdf_font_file);
+  if (!font.LoadFont(bdf_font_file.c_str())) {
+    fprintf(stderr, "Couldn't load font '%s'\n", bdf_font_file.c_str());
     return 1;
   }
   rgb_matrix::Font *outline_font = NULL;
@@ -126,43 +46,40 @@ int main(int argc, char *argv[]) {
       outline_font = font.CreateOutlineFont();
   }
 
+  // Error checking on the brightness
   if (brightness < 1 || brightness > 100) {
     fprintf(stderr, "Brightness is outside usable range.\n");
     return 1;
   }
 
-  RGBMatrix *matrix = rgb_matrix::CreateMatrixFromOptions(matrix_options,
-                                                          runtime_opt);
+  // Create the matrix
+  RGBMatrix::Options matrix_options;
+  rgb_matrix::RuntimeOptions runtime_opt;
+  RGBMatrix *matrix = rgb_matrix::CreateMatrixFromOptions(matrix_options, runtime_opt);
   if (matrix == NULL)
     return 1;
 
   matrix->SetBrightness(brightness);
 
-  const bool all_extreme_colors = (brightness == 100)
-      && FullSaturation(color)
-      && FullSaturation(bg_color)
-      && FullSaturation(outline_color);
-  if (all_extreme_colors)
-      matrix->SetPWMBits(1);
-
   const int x = x_orig;
   int y = y_orig;
 
+  // Create the canvas and buffer
   FrameCanvas *offscreen = matrix->CreateFrameCanvas();
-
   char text_buffer[256];
   struct timespec next_time;
   next_time.tv_sec = time(NULL);
   next_time.tv_nsec = 0;
   struct tm tm;
 
+  // Set up signal handlers
   signal(SIGTERM, InterruptHandler);
   signal(SIGINT, InterruptHandler);
 
-  // Continue as long as there is no interrupt
+  // Loop until we receive a signal
   while (!interrupt_received) {
       localtime_r(&next_time.tv_sec, &tm);
-      strftime(text_buffer, sizeof(text_buffer), time_format, &tm);
+      strftime(text_buffer, sizeof(text_buffer), time_format.c_str(), &tm);
       offscreen->Fill(bg_color.r, bg_color.g, bg_color.b);
       if (outline_font) {
           rgb_matrix::DrawText(offscreen, *outline_font,
@@ -189,18 +106,4 @@ int main(int argc, char *argv[]) {
 
   write(STDOUT_FILENO, "\n", 1);  // Create a fresh new line after ^C on screen
   return 0;
-}
-
-// Draw text along the bottom of the screen
-void draw_text(const char *text)
-{
-  // The regular text. Unless we already have filled the background with
-  // the outline font, we also fill the background here.
-  rgb_matrix::DrawText(canvas, font, x, y + font.baseline(),
-                       color, outline_font ? NULL : &bg_color, line,
-                       letter_spacing);
-
-  // Finished. Shut down the RGB matrix.
-  canvas->Clear();
-  delete canvas;
 }
