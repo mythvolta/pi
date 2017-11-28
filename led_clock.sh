45d5c4949fe49e0dbee690144c23d584c0235713
Pi Volta <mythvolta@github.com>
2017-11-27 19:05:04 -0500

    Call mythclock, and log the output.

--- led_clock.sh
+++ led_clock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Run the clock on the LED matrix
-LED=~pi/code/rpi-rgb-led-matrix
+P="~pi/toolbox/code/mythclock"
 
 # Ensure that only one version of this script runs
 SCRIPT=`basename $0`
@@ -12,6 +12,4 @@ done
 
 # Kill all old instances of mythclock before starting this one
 sudo pkill -9 -f mythclock
-#sudo $LED/examples-api-use/clock -f $LED/fonts/6x13B.bdf -b3 -x1 -y-1
-#sudo $LED/examples-api-use/clock -f $LED/fonts/6x13B.bdf -b10 -d '%H%M' --led-rows=16 -S1 -y-2 -x0 -C 128,0,0 &
-sudo $LED/eric/mythclock -f $LED/fonts/6x13B.bdf -b10 -d '%H%M' --led-rows=16 -S1 -y-2 -x0 -C 128,0,0
+sudo $P/mythclock >> $P/mythclock.log
