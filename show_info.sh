9239d1923dcd7308acd21f52ef8ec32bfb4b0951
Pi Volta <mythvolta@github.com>
2017-12-31 15:31:48 -0500

    Show lsb information, and more concise ssh key info.

--- show_info.sh
+++ show_info.sh
@@ -7,9 +7,13 @@ IP=`hostname -I`
 echo "$HOST @ $IP"
 uname -a | head
 echo
+lsb_release -a
+echo
 echo "sudo apt-get -y autoremove"
 echo "sudo apt-get -y autoclean"
 echo "sudo apt-get -y clean"
 echo
-cat ~/.ssh/authorized_keys
+SSH=~/.ssh/authorized_keys
+echo ${SSH}:
+grep -Eo ' [^ ]+$' $SSH
 echo
