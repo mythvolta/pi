2bc5159024c5bae9b537ec0d7c7011ef2f655d7d
Pi Volta <mythvolta@github.com>
2018-08-18 16:15:52 -0400

    Add possibility of a second LED. Also decrease fan temperature to 65C.

--- code/include/gpio_button_led_fan.h
+++ code/include/gpio_button_led_fan.h
@@ -15,17 +15,18 @@
 #include "log_date.h"
 
 // Easily disable the LED or the fan
-#define HAS_LED
+#define HAS_LED1
+#define HAS_LED2
 #define HAS_FAN
 const std::string ELEMENT_MAC = "bc:ae:c5:dc:5a:e2";  // For sodium (na)
 //const std::string ELEMENT_MAC = "bc:ae:c5:dc:5a:98";  // For magnesium (mg)
 
 // There is one LED, two buttons, and a fan
-#define GPIO_LED    18   // The LED is connected to GPIO18 (pin 12, WPi 1) in ALT5 mode for PWM
-#define GPIO_BTN    27   // Button A is connected to GPIO27 (pin 13/gnd 14, WPi 2)
-#define GPIO_FAN    23   // The fan is connected to GPIO24 (pin 16/gnd 14, WPi 4)
-//#define GPIO_FAN    24   // The fan is connected to GPIO24 (pin 18/gnd 20, WPi 4)
-#define GPIO_IR     25   // The IR receiver is connected to GPIO25 (pin 18, WPi5)
+#define GPIO_LED1   18   // The first LED is connected to GPIO18 (pin 12, WPi 1) in ALT5 mode for PWM
+#define GPIO_LED2   13   // The second LED is connected to GPIO13 (pin 33, WPi 23) in ALT0 mode for PWM
+#define GPIO_BTN    27   // Button A is connected to GPIO27 (pin 13/gnd 1, WPi 2)
+#define GPIO_FAN    23   // The fan is connected to GPIO23 (pin 16/gnd 14, WPi 4)
+#define GPIO_IR     24   // The IR receiver is connected to GPIO24 (pin 18, WPi 5)
 
 // Default to the internal LED if we have to
 #define SYSFS_LED   "/sys/class/leds/led0"
@@ -51,10 +52,10 @@ int power_off();
 
 // Control the LED
 int led_off();
-#ifdef HAS_LED
+#ifdef HAS_LED1
 int led_cycle_brightness();
 void led_pulse(const unsigned int num_pulses);
-#endif // HAS_LED
+#endif // HAS_LED1
 void led_blink();
 
 // Turn the fan on or off based on CPU temperature
