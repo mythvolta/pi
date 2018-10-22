7c3f8138e179d80287b1e20c936f9efd7c01c98c
Pi Volta <mythvolta@github.com>
2018-10-22 12:00:39 -0400

    Comment out UQM link, since it no longer works. Remove all old launching.jpg and autoconfig links.

--- fix_retropie_links.sh
+++ fix_retropie_links.sh
@@ -58,19 +58,12 @@ replace_link /home/pi/retropie/roms/psx-hidden /home/pi/media/games/roms/psx-hid
 
 # Replace these links
 replace_link /home/pi/retropie /home/pi/RetroPie
-#replace_link /opt/retropie/configs/all/autoconf.cfg /home/pi/retropie/configs/autoconf.cfg
 replace_link /opt/retropie/configs/all/emulationstation /home/pi/retropie/emulationstation
-#replace_link /opt/retropie/configs/all/retroarch/autoconfig /home/pi/retropie/configs/autoconfig
 replace_link /opt/retropie/configs/all/retroarch.cfg /home/pi/retropie/configs/retroarch.cfg
-#replace_link /opt/retropie/configs/all/retroarch-joypads /opt/retropie/configs/all/retroarch/autoconfig
 replace_link /opt/retropie/configs/all/retroarch/screenshots /home/pi/retropie/screenshots
-#replace_link /opt/retropie/configs/nes/launching.jpg /home/pi/retropie/launching/nes.jpg
 replace_link /opt/retropie/configs/nes/retroarch.cfg /home/pi/retropie/configs/retroarch_nes.cfg
-#replace_link /opt/retropie/configs/psp/PSP/GAME /home/pi/RetroPie/roms/psp
-#replace_link /opt/retropie/configs/psx/launching.jpg /home/pi/retropie/launching/psx.jpg
 replace_link /opt/retropie/configs/psx/retroarch.cfg /home/pi/retropie/configs/retroarch_psx.cfg
-#replace_link /opt/retropie/configs/snes/launching.jpg /home/pi/retropie/launching/snes.jpg
 replace_link /opt/retropie/configs/snes/retroarch.cfg /home/pi/retropie/configs/retroarch_snes.cfg
 
 # UQM
-replace_link /home/pi/.uqm /home/pi/retropie/uqm
+#replace_link /home/pi/.uqm /home/pi/retropie/uqm
